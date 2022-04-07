7d2841d63611a4efe66ac566168982c0c46186bd
Kirty Meena <kirtymeena@gmail.com>
2022-04-07 14:32:33 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Card from "./components/Card";
 
 function App() {
+  const visitProfile = ()=>{
+    window.location.href="https://www.frontendmentor.io/profile/kirtymeena"
+  }
+  const visitWebsite=()=>{
+    window.location.href="https://www.frontendmentor.io/home"
+  }
   return (
     <main>
       <Card/>
+      <footer>
+      <p>Challenge by <span onClick={visitWebsite}>Frontend Mentor</span>. Coded by <span onClick={visitProfile}>Kirty Meena</span></p>.
+      </footer>
     </main>
   );
 }
